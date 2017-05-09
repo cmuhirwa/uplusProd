f9743752896781c779847343ac6f3f456477b267
Clement <muhirwaclement@gmail.com>
2017-05-09 10:17:10 +0200

    dbchange

--- db.php
+++ db.php
@@ -1,17 +1,17 @@
 <?php  
-	$db = new mysqli("localhost", "root", "" , "uplus");
+	$db = new mysqli("localhost", "root", "clement123" , "uplus");
 	
 	if($db->connect_errno){
 		die('Sorry we have some problem with the General Database!');
 	}
 	
-	$investdb = new mysqli("localhost", "root", "" , "commerce_db");
+	$investdb = new mysqli("localhost", "root", "clement123" , "commerce_db");
 	
 	if($investdb->connect_errno){
 		die('Sorry we have some problem with the Investment Database!');
 	}
 
-	$outCon  = new mysqli("localhost", "root", "" , "rtgs");
+	$outCon  = new mysqli("localhost", "root", "clement123" , "rtgs");
 	if($outCon->connect_errno){
 		die('Sorry we have some problem with the RTGS Database!');
 	}
