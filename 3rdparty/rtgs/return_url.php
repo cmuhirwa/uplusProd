5e2c992da5f1d858d08be02cf207b4968618418b
Clement <muhirwaclement@gmail.com>
2017-05-20 11:23:49 +0200

    return visa

--- 3rdparty/rtgs/return_url.php
+++ 3rdparty/rtgs/return_url.php
@@ -13,14 +13,25 @@ require __DIR__ . '/function.php';
 $transaction['status'] = getInput('vpc_TxnResponseCode');
 $transaction['key']    = getInput('vpc_TransactionNo');
 $transaction['message'] = getInput('vpc_Message');
+$transaction['currency'] = getInput('vpc_Currency');
+$transaction['merchant'] = getInput('vpc_Merchant');
+$transaction['amount'] = getInput('vpc_Amount');
+$transaction['orderInfo'] = getInput('vpc_OrderInfo');
 
 // $reference = getInput('vpc_MerchTxnRef');
 // Get order from the database by the `$reference` generated random number in the request process
 
 if($transaction['status'] == "0" && $transaction['message'] == "Approved") {
 
-    // Save transaction information in the database
-    // Display transaction details
+  echo $transaction['amount'].'<br/>';
+  echo $transaction['currency'].'<br/>';
+  echo $transaction['message'].'<br/>';
+  echo $transaction['merchant'].'<br/>';
+  echo $transaction['key'].'<br/>';
+  echo $transaction['orderInfo'].'<br/>';
+  session_start();
+  echo $_SESSION['orderInfo'].'<br/>';
+
 } else {
 
     // Display error
