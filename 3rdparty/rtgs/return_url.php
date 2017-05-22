319c19426eea3e1e05973b934c2e8c9a9db50180
Clement <muhirwaclement@gmail.com>
2017-05-22 13:54:01 +0200

    show errors

--- 3rdparty/rtgs/return_url.php
+++ 3rdparty/rtgs/return_url.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 0);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 0);
 /**
  * A sample code to illustrate how a payment response will be handled.
  *
